4fce709bbf674d8eeab1e578605a78daa7ff6026
peter.voznek <peter.voznek@attrecto.com>
2024-09-13 16:14:40 +0200

    init

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react";
+import classNames from "classnames";
+
+import classes from "./Navbar.module.scss";
+
+const Navbar: FC = () => {
+  return (
+    <nav className={classNames("navbar", [classes.Navbar])}>
+      <div className="ps-3 d-flex flex-grow-1">Welcome to Attrecto Academy</div>
+    </nav>
+  );
+};
+
+export default Navbar;
